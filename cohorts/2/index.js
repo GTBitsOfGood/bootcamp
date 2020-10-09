4e6e6d61b4e9f92a413230ceb8a49ab55c1b6791
Lidya <laberra@gatech.edu>
2020-10-08 20:13:15 -0400

    feat: basic js for cohort page

---
+++ cohorts/2/index.js
@@ -0,0 +1,26 @@
+function Member(name, fun_fact) {
+    this.name = name;
+    this.fun_fact = fun_fact;
+}
+
+const member1 = new Member("lidya", "favorite book: Classics of Western Philosophy");
+const member2 = new Member("alice", "wears lolita-fashion casually");
+const member3 = new Member("seife", "insert fact");
+const member4 = new Member("shree", "insert fact");
+const member5 = new Member("evan", "insert fact");
+const member6 = new Member("vivian", "insert fact");
+
+var cohort = {
+    name: "Terrible Twos",
+    motto: "Send help",
+    members: [
+        member1,
+        member2,
+        member3,
+        member4,
+        member5,
+        member6,
+    ] 
+}
+
+console.log(cohort);
