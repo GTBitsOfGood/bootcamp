60a26a689bb114532d9c093349e92400afe264cc
Vivian Carlson <vivian.s.carlson@gmail.com>
2020-10-13 19:16:39 -0400

    Added my fun fact

--- cohorts/2/index.js
+++ cohorts/2/index.js
@@ -8,7 +8,7 @@ const member2 = new Member("alice", "wears lolita-fashion casually");
 const member3 = new Member("seife", "insert fact");
 const member4 = new Member("shree", "insert fact");
 const member5 = new Member("evan", "insert fact");
-const member6 = new Member("vivian", "insert fact");
+const member6 = new Member("vivian", "I paint Warhammer minis as a hobby");
 
 var cohort = {
     name: "Terrible Twos",
