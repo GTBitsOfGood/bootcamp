68d635cf9648aa45f8a3f4cc9475efb1ca4cd827
lyann1999 <70612133+lyann1999@users.noreply.github.com>
2020-10-13 20:10:14 -0400

    modified index css and js

---
+++ cohorts/3/index.js
@@ -0,0 +1,41 @@
+console.log("hello")
+
+function Cohort(name, motto) {
+    this.name = name;
+    this.motto = motto
+}
+const myCohort = {
+    name: 'Corgi',
+    motto: 'Woof! XOXO',
+
+}
+function Members(name, funFact) {
+    this.name = name;
+    this.motto = funFact;
+
+}
+
+const myMember1 = {
+    name: 'Rita',
+    funFact: 'I like building Legos'
+}
+
+const myMember2 = {
+    name: 'Lyann',
+    funFact: "I hate boba or any kind of tea"
+}
+
+const myMember2 = {
+    name: 'Anusha',
+    funFact: ""
+}
+
+const myMember2 = {
+    name: 'Fareeda',
+    funFact: ""
+}
+
+
+console.log(myCohort)
+console.log(myMember1)
+console.log(myMember2)
