68893e0b300146398e835196d733c9d6e1b5c07d
Evan Montoya <you@example.com>
2020-10-15 19:23:28 -0400

    added evan's fun fact

--- cohorts/2/index.js
+++ cohorts/2/index.js
@@ -7,7 +7,7 @@ const member1 = new Member("lidya", "favorite book: Classics of Western Philosop
 const member2 = new Member("alice", "wears lolita-fashion casually");
 const member3 = new Member("seife", "insert fact");
 const member4 = new Member("shree", "insert fact");
-const member5 = new Member("evan", "insert fact");
+const member5 = new Member("evan", "connoisseur of water");
 const member6 = new Member("vivian", "I paint Warhammer minis as a hobby");
 
 var cohort = {
