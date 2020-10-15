17f529c96619ea4e2a20b0c6021ea8b0d7b8d7b5
ruchipatel1 <ruchipatel@gatech.edu>
2020-10-15 19:17:47 -0400

    added javascript to populate text

--- cohorts/4/index.js
+++ cohorts/4/index.js
@@ -12,8 +12,13 @@ members.push(new Person("Ruchi", "Ruchi's fact"));
 
 var cohort = {
 	name: 'KAIRScares',
-	motto: '___our motto___',
+	motto: 'we KAIR about development but it Scares us too',
 	team: members
 };
 
-console.log(cohort)
+for (const member of members) {
+	document.getElementById(member.name).innerText = member.name;
+}
+
+document.getElementById('motto').innerText = cohort.motto;
+document.getElementById('group-name').innerText = cohort.name;
