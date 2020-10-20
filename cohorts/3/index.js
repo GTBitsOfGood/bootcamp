690753ec7014c72a264328f6f79b26b5d5cedd6b
aprasad888 <aprasad8008@gmail.com>
2020-10-20 16:59:19 -0400

    new changes

--- cohorts/3/index.js
+++ cohorts/3/index.js
@@ -25,15 +25,27 @@ const myMember2 = {
     funFact: "I hate boba or any kind of tea"
 }
 
-const myMember2 = {
+const myMember3 = {
     name: 'Anusha',
     funFact: ""
 }
 
-const myMember2 = {
+const myMember4 = {
     name: 'Fareeda',
     funFact: ""
 }
+
+document.getElementById("title").innerHTML = "COHORT CORGI";
+var str = "Motto:";
+document.getElementById("motto").innerHTML = str.bold() + " Woof! XOXO";
+document.getElementById("images").getElementsByClassName("caption")[0].innerHTML = "Fareeda";
+document.getElementById("images").getElementsByClassName("caption")[1].innerHTML = "Lyann";
+document.getElementById("images").getElementsByClassName("caption")[2].innerHTML = "Kimberly";
+document.getElementById("images").getElementsByClassName("caption")[3].innerHTML = "Anusha";
+document.getElementById("images").getElementsByClassName("caption")[4].innerHTML = "Rita";
+document.getElementById("images").getElementsByClassName("caption")[5].innerHTML = "Shruthi";
+
+
 
 
 console.log(myCohort)
